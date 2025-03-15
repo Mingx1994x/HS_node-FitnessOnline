c7f064b51b1bd43d3e76ffceb8b1d59e87e79f2c
Mingx1994x <mggxmingx@gmail.com>
2025-03-15 17:05:32 +0800

    commit w5作業

---
+++ utils/validate.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+  isUndefined: (value) => {
+    return value === undefined
+  },
+  isNotValidString: (value) => {
+    return typeof value !== "string" || value.trim().length === 0 || value === ""
+  },
+  isNotValidInteger: (value) => {
+    return !Number.isInteger(value) || value === 0
+  },
+  isNotValidUserName: (value) => {
+    //最少2個字，最多10個字，不可包含任何特殊符號與空白
+    const userNameRegex = /^[\u4e00-\u9fa5A-Za-z0-9]{2,10}$/;
+    return !userNameRegex.test(value)
+  },
+  isNotValidEmail: (value) => {
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    return !emailRegex.test(value)
+  },
+  isNotValidUserPassword: (value) => {
+    //需要包含英文數字大小寫，最短8個字，最長16個字
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,16}$/;
+    return !passwordRegex.test(value)
+  },
+  isNotValidUrl: (value) => {
+    return !value.startsWith('https')
+  },
+  isNotValidImg: (value) => {
+    let imgFormat = value.split('.').pop();
+    return !((imgFormat === 'png' || imgFormat === 'jpg'))
+  }
+
+}
